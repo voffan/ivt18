321c2162b66c38c2de57f4c1df6d2ec47ee67e60
Ambros52 <43155893+Ambros52@users.noreply.github.com>
2021-01-31 23:31:43 +0900

    form

---
+++ gallery/gallery/Expo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace gallery
+{
+    class Expo
+    {      
+        public int ExpoId { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        [ForeignKey("Place")]
+        public int PlaceId { get; set; }
+        public Place Place { get; set; }
+
+        public ICollection<PictureId> Pictures { get; set; }
+        public Gallery gallery { get; set; }
+    }
+}
