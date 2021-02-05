9802fa04dfac16e817a23e0d26cfc0a811a8ffe7
Mikhail Petrov <43157772+petrovmm@users.noreply.github.com>
2021-02-05 11:44:13 +0900

    Add files via upload

---
+++ pi1/pi1/Division.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Core;
+
+
+namespace bugtracking
+{
+    class Division
+    {
+        [Foreign Key("Name")]
+        public string Name { get; set; }
+    }
+}
