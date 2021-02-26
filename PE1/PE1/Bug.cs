ee9f875ceac8480b259a1a272562478f726df394
NikolaevEM <43157756+NikolaevEM@users.noreply.github.com>
2021-02-26 12:29:08 +0900

    Add files via upload

---
+++ PE1/PE1/Bug.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace PE1
+{
+    public enum BugStatus {New, Closed}
+    class Bug
+    {
+        [Key]
+        public int BugId { get; set; }
+        public BugType BugType { get; set; }
+        public string Description { get; set; }
+        public DateTime LastFixedDate { get; set; }
+        public DateTime AddedDate { get; set; }
+        public StatusBug Status { get; set; }
+
+        public virtual ICollection<Fix> Fixes { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+
+        public int EmplyeeId { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
+        
+
+    }
+}
---
+++ PE1/PE1/Fix.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace PE1
+{
+    class Fix
+    {
+        [Key]
+        public int FixId { get; set; }
+        public string Description { get; set; }
+        public int Rank { get; set; }
+        public DateTime Date { get; set; }
+
+        public int BugId { get; set; }
+        [ForeignKey("BugId")]
+        public Bug Bug { get; set; }
+
+        public int EmployeeId { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
+    }
+}
