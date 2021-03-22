6c1869736c495030f92ca36bd2f5aa1e10c620e0
uufswe21 <43157713+uufswe21@users.noreply.github.com>
2021-03-22 12:12:05 +0900

    database
    add-migration
    update-database

---
+++ carton/carton/Migrations/202103220242262_UpdateEmployee.cs
@@ -0,0 +1,28 @@
+﻿namespace carton.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateEmployee : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Employee", "Surname", c => c.String());
+            AddColumn("dbo.Employee", "Name", c => c.String());
+            AddColumn("dbo.Employee", "Fathername", c => c.String());
+            AddColumn("dbo.Employee", "Login", c => c.String());
+            AddColumn("dbo.Employee", "Password", c => c.String());
+            DropColumn("dbo.Employee", "FullName");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Employee", "FullName", c => c.String());
+            DropColumn("dbo.Employee", "Password");
+            DropColumn("dbo.Employee", "Login");
+            DropColumn("dbo.Employee", "Fathername");
+            DropColumn("dbo.Employee", "Name");
+            DropColumn("dbo.Employee", "Surname");
+        }
+    }
+}
