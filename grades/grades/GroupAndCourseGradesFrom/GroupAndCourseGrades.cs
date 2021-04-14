148c105ede2bc4f5d41259e4e6af3d733ae04710
Tinck <glushkov2000@bk.ru>
2021-04-14 21:58:22 +0900

    Teacher grades

---
+++ grades/grades/GroupAndCourseGradesFrom/GroupAndCourseGrades.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace grades
+{
+    public partial class GroupAndCourseGrades : Form
+    {
+        private Context _context;
+        private GroupAndCourseGradesLogic _logic;
+        private Staff _user;
+
+        //public GroupAndCourseGrades(Context context)
+        public GroupAndCourseGrades()
+        {
+            InitializeComponent();
+
+            _context = new Context();
+            _logic = new GroupAndCourseGradesLogic();
+            _user = (from usr in _context.Staff
+                     where usr.PersonId == 17
+                     select usr).Single();
+
+            LoadCoursesList();
+            LoadGroupList();
+        }
+
+        private void LoadCoursesList()
+        {
+            courseComboBox.DataSource = _logic.GetCoursesList(_context, _user);
+        }
+
+        private void LoadGroupList()
+        {
+            groupComboBox.DataSource = _logic.GetGroupList(_context, _user);
+        }
+    }
+}
---
+++ grades/grades/GroupAndCourseGradesFrom/GroupAndCourseGradesLogic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grades
+{
+    class GroupAndCourseGradesLogic
+    {
+        internal List<dynamic> GetCoursesList(Context context, Staff user)
+        {
+            return (from courses in context.Courses
+                    where courses.StaffId == user.PersonId
+                    select courses.Subject.Name).Distinct().ToList<dynamic>();
+        }
+
+        internal List<dynamic> GetGroupList(Context context, Staff user)
+        {
+            return (from groups in context.Groups
+                    where groups.StaffId == user.PersonId
+                    select new
+                    {
+                        groups.Year
+                    }
+                    ).ToList<dynamic>();
+        }
+    }
+}
