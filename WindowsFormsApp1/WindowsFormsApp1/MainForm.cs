e9ef7a6bf0b4e0299c610a1fb1d1ce79a1cd103b
Ivanov Victor <viktor.sosnizky@gmail.com>
2021-04-23 14:14:27 +0900

    mainm

--- WindowsFormsApp1/WindowsFormsApp1/MainForm.cs
+++ WindowsFormsApp1/WindowsFormsApp1/MainForm.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
