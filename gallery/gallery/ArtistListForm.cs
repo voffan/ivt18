aaf64f61d76878fe9962c2423cb7aa7fa2b1b921
VeniaminVV <vinichven@gmail.com>
2021-06-25 02:19:34 +0900

    searching

--- gallery/gallery/ArtistListForm.cs
+++ gallery/gallery/ArtistListForm.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -26,6 +28,50 @@ namespace gallery
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection ConStr = new SqlConnection(ConfigurationManager.ConnectionStrings["ConStr"].ConnectionString))
+                {
+                    if (ConStr.State == ConnectionState.Closed)
+                        ConStr.Open();
+                    using (DataTable dt = new DataTable("Artists"))
+                    {
+                        using (SqlCommand cmd = new SqlCommand("select *from artist where artistid=@artistid or fullname like @fullname", ConStr))
+                        {
+                            cmd.Parameters.AddWithValue("artistid", txtSearch.Text);
+                            cmd.Parameters.AddWithValue("fullname", string.Format("%{0}%", txtSearch.Text));
+                            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                            adapter.Fill(dt);
+                            dataGridView1.DataSource = dt;
+                            lblTotal.Text = $"Всего совпадений: {dataGridView1.RowCount}";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtSearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+                btnSearch.PerformClick(); 
         }
     }
 }
--- gallery/gallery/PictureListForm.cs
+++ gallery/gallery/PictureListForm.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -176,6 +178,57 @@ namespace gallery
 
         private void label1_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection ConStr = new SqlConnection(ConfigurationManager.ConnectionStrings["ConStr"].ConnectionString))
+                {
+                    if (ConStr.State == ConnectionState.Closed)
+                        ConStr.Open();
+                    using(DataTable dt = new DataTable("Pictures"))
+                    {
+                        using(SqlCommand cmd = new SqlCommand("select *from picture where pictureid=@pictureid or name like @name", ConStr))
+                        {
+                            cmd.Parameters.AddWithValue("pictureid", txtSearch.Text);
+                            cmd.Parameters.AddWithValue("genreid", txtSearch.Text);
+                            cmd.Parameters.AddWithValue("name", string.Format("%{0}%", txtSearch.Text));
+                            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                            adapter.Fill(dt);
+                            dataGridView1.DataSource = dt;
+                            lblTotal.Text = $"Всего совпадений: {dataGridView1.RowCount}";
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            btnSearch.PerformClick();
 
         }
     }
